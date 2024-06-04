7ff50b9f8e44b0f167ff3cdf96c365c42b788cfc
root <2560bed>
2024-06-04 01:22:04 -0700

    consale

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """Defines the Amenity class."""
-
-
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Amenity class for AirBnB project
+    """Represent an amenity.
 
-    name - empty string
+    Attributes:
+        name (str): The name of the amenity.
     """
 
     name = ""
